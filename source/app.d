2cfaaa59c6a6193c287550f0fa02d1b1de742245
David Shaked <ds2643@columbia.edu>
2016-11-16 04:40:07 -0500

    tests, mutation for primitive types

--- source/app.d
+++ source/app.d
@@ -1,6 +1,146 @@
+// mutate arbitrary data
+// overloaded mutation operation for primitive types
 import std.stdio;
+import std.random;
+import std.traits;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+bool mutate(bool a) {
+    return (uniform(0,2) > 0);
+}
+
+byte mutate(byte a) {
+    return cast(byte) uniform(0, 256);  // 8 bits
+}
+
+ubyte mutate(ubyte a) {
+    return cast(ubyte) uniform(0, 256); // 8 bits
+}
+
+short mutate(short a) {
+    return cast(short) uniform(0, 65536); // 16 bits
+}
+
+ushort mutate(ushort a) {
+    return cast(ushort) uniform(0, 65536); // 16 bits
+}
+
+int mutate(int a) {
+    return cast(int) uniform(0, 4294967296); // 32 bits
+}
+
+uint mutate(uint a) {
+    return castu(uint) uniform(0, 4294967296); // 32 bits
+}
+
+long mutate(long a) {
+    return cast(long) uniform(0, 18446744073709551616); // 64 bits
+}
+
+ulong mutate(ulong a) {
+    return cast(ulong) uniform(0, 18446744073709551616); // 64 bits
+}
+
+float mutate(float a) {
+    return cast(float) uniform(0, 4294967296); // 32 bits
+}
+
+/*
+ifloat mutate(ifloat a) {
+
+}
+
+cfloat mutate(cfloat a) {
+
+}
+*/
+
+double mutate(double a) {
+    return cast(long) uniform(0, 18446744073709551616);
+}
+
+/*
+idouble mutate(idouble a) {
+
+}
+
+cdouble mutate(cdouble a) {
+
+}
+
+real mutate(real a) {
+
+}
+
+ireal mutate(ireal a) {
+
+}
+
+creal mutate(creal a) {
+
+}
+
+char mutate(char a) {
+
+}
+
+wchar mutate(wchar a) {
+
+}
+
+dchar mutate(dchar a) {
+
+}
+*/
+
+unittest { // TODO: add values
+    assert(isBoolean(mutate(true))); // boolean
+
+    auto randByte = mutate(cast(byte) 0); // byte
+    assert((typeof(randByte) == byte) && isSigned(randByte));
+
+    auto randUbyte = mutate(cast(ubyte) 0); // ubyte
+    assert((typeof(randUbyte) == ubyte) && isUnsigned(randUbyte));
+
+    auto randShort = mutate(cast(short) 0); // short
+    assert((typeof(randShort) == short) && isSigned(randShort));
+
+    auto randUshort = mutate(cast(ushort) 0); // ushort
+    assert((typeof(randUshort) == ushort) && isUnsigned(randUshort));
+
+    auto randInt = mutate(cast(int) 0); // int
+    assert(isIntegral(randInt) && isSigned(randInt));
+
+    auto randUint = mutate(cast(uint) 0); // unit
+    assert(isIntegral(randUint) && isUnsigned(randUint));
+
+    auto randLong = mutate(cast(long) 0L); // long
+    assert((typeof(randLong) == long) && isUnsigned(randLong));
+
+    auto randUlong = mutate(cast(ulong) 0);// ulong
+    assert((typeof(randUlong) == long) && isUnsigned(randUlong));
+
+    auto randFloat = mutate(cast(float) float.nan); // float
+    assert(isFloatingPoint(mutate(1.0F)); // float
+
+    assert(isSomeChar(mutate(0xFF))); // char
+
+    auto randDchar = mutate(0x0000FFFF); // dchar
+    assert(typeof(randDchar) == dchar);
+
+    auto randWchar = mutate(0xFFFF); // wchar
+    assert(typeof(randWchar) == wchar);
+
+    // double double.nan
+    // real real.nan
+    // ifloat
+    // idouble
+    // ireal
+    // cfloat
+    // cdouble
+    // creal
+    // char
+}
+
+void main() {
+    return 0;
 }
---
+++ source/fisr.d
@@ -0,0 +1,21 @@
+import std.stdio;
+
+float fisr(float n) {
+    // in good code style, move these initializations locally to their point of use
+    float x2 = n * 0.5F;
+    float y = n;
+    float threeHalves = 1.5F;
+    long i = * cast(long*) &y;
+    i = 0x5f3759df - ( i >> 1 );
+    y = * cast(float*) &i;
+    y = y * (1.5F - (x2 * y * y));
+    return y;
+}
+
+void main() {
+    write("n? ");
+    float n;
+    readf(" %s", &n);
+    float result = fisr(n); // auto?
+    writeln("Fast inverse square root of ", n, " is ", result);
+}
