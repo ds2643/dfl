e55dd8f2b64c9dc215a1b86bc0aacb1a79405585
David Shaked <ds2643@columbia.edu>
2016-11-17 05:08:24 -0500

    modularized

--- source/mutate/complex.d
+++ source/mutate/complex.d
@@ -2,3 +2,22 @@ module mutate.complex;
 
 // mutate data of derived type
 
+/* STANDARD */
+
+// pointer
+// array
+// strings
+// associative array
+// function
+// delegate
+
+/* USER DEFINED */
+
+// alias
+// enum
+// struct
+// union
+// class
+
+// base types
+// https://dlang.org/spec/type.html
