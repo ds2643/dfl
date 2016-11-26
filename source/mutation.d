878134b9a7f6476edcc856f954b11a2ca531f253
David Shaked <ds2643@columbia.edu>
2016-11-25 19:18:46 -0500

    mutation tests updated but not complete

--- source/mutation.d
+++ source/mutation.d
@@ -12,22 +12,20 @@ import std.algorithm.iteration;
 
 // PRIMITIVE TYPES
 
-auto mutate(T)(T value){
-    // integral and floating point numbers and characters
-    if(is(T == bool)) {
-        return (uniform(0,2) > 0);
-    }
-    else if (__traits(isIntegral, value))  {
-        return cast(T) uniform!T();
-    }
-    else if (__traits(isFloating, value)) {
+auto mutate(bool value) {
+    return (uniform(0,2) > 0);
+}
+
+auto mutate(T)(T value) if (isFloatingPoint!T) {
         // floating points cannot be generated with the uniform template like integral values
-        if (is(T == float)) { return cast(T) uniform!uint(); }
-        else if (is(T == real)) { return cast(T) uniform!long(); }
-        else if (is(T == double)) { return cast(T) uniform!ulong(); }
-        // TODO: add logic for complex and imaginary variants
-    }
-    // TODO: provide catch all case
+    if (is(T == float)) { return cast(T) uniform!uint(); }
+    else if (is(T == real)) { return cast(T) uniform!long(); }
+    else if (is(T == double)) { return cast(T) uniform!ulong(); }
+    // TODO: add support for complex and imaginary floating point numbers
+}
+
+auto mutate(T)(T value) if (isIntegral!T) {
+     return cast(T) uniform!T();
 }
 
 // DERIVED TYPES
@@ -62,33 +60,44 @@ auto mutate(T, U)(T[U] value) {
 
 unittest {
     // test mutation of boolean and integral types
-    auto booleanData = true;
-    assert(isBoolean(mutate!(bool)(booleanData))); // boolean
-    /*
-    auto randByte = mutate!(byte)(cast(byte) 0); // byte
-    assert((typeof(randByte).stringof == "byte") && __traits(isSigned, randByte));
+    bool booleanData = true;
+    //assert(isBoolean(mutate!(booleanData))); // boolean
+    assert(isBoolean!(typeof(mutate(booleanData)))); // boolean
 
-    auto randUbyte = mutate!(ubyte)(cast(ubyte) 0); // ubyte
-    assert((typeof(randUbyte).stringof == "ubyte") && __traits(isUnsigned, randUbyte));
+    // TODO: extend to other integral types
+    assert(isIntegral!(typeof(mutate(100))));
 
-    auto randShort = mutate!(short)(cast(short) 0); // short
-    assert((typeof(randShort).stringof == "short") && __traits(isSigned, randShort));
+    //auto randByte = mutate!(byte)(cast(byte) 0); // byte
+    //assert((typeof(randByte).stringof == "byte") && __traits(isSigned, randByte));
 
-    auto randUshort = mutate!(ushort)(cast(ushort) 0); // ushort
-    assert((typeof(randUshort).stringof == "ushort") && __traits(isUnsigned, randUshort));
+    //auto randUbyte = mutate!(ubyte)(cast(ubyte) 0); // ubyte
+    //assert((typeof(randUbyte).stringof == "ubyte") && __traits(isUnsigned, randUbyte));
 
-    auto randInt = mutate!(int)(cast(int) 0); // int
-    assert(__traits(isIntegral, randInt).stringof && __traits(isSigned, randInt));
+    auto shortData = cast(short) 0;
+    auto mutatedShort = mutate(shortData);
+    assert(is(typeof(mutatedShort) == short));
 
-    auto randUint = mutate!(uint)(cast(uint) 0); // unit
-    assert(__traits(isIntegral, randUint).stringof && __traits(isUnsigned, randUint));
+    auto ushortData = cast(ushort) 0;
+    auto mutatedUShort = mutate(ushortData);
+    assert(is(typeof(mutatedUShort) == ushort));
 
-    auto randLong = mutate!(long)(cast(long) 0L); // long
-    assert((typeof(randLong).stringof == "long") && __traits(isUnsigned, randLong));
+    auto intData = (cast(int) 0);
+    auto mutatedInt = mutate(intData);
+    assert(is(typeof(mutatedInt) == int));
 
-    auto randUlong = mutate!(ulong)(cast(ulong) 0);// ulong
-    assert((typeof(randUlong).stringof == "long") && __traits(isUnsigned, randUlong));
+    auto uIntData = (cast(uint) 0);
+    auto mutatedUInt = mutate(uIntData);
+    assert(is(typeof(mutatedUInt) == uint));
 
+    auto longData = (cast(long) 0);
+    auto mutatedLong = mutate(longData);
+    assert(is(typeof(mutatedLong) == long));
+
+    auto ulongData = (cast(ulong) 0);
+    auto mutatedULong = mutate(ulongData);
+    assert(is(typeof(mutatedULong) == ulong));
+
+    // TODO: fix these tests
     char charData = 0xFF;
     assert(isSomeChar(mutate!(char)(charData))); // char
 
@@ -100,6 +109,7 @@ unittest {
     auto randWchar = mutate!(typeof(wcharData))(wcharData); // wchar
     assert(typeof(randWchar).stringof == "wchar");
 
+    /*
     // non-discrete numeric values
     auto randFloat = mutate!(cast(float) float.min_normal); // float... note that choice of min_normal property arbitrarily (for instance, over min_exp or .min_10_exp)
     assert(isFloatingPoint(mutate(1.0F)));
